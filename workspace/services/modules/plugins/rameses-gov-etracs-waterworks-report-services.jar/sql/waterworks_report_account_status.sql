a78918fb24c789100bb30e20016c89b878fecc53
worgflores <worgflores@gmail.com>
2019-06-01 13:52:17 +0800

    updated

---
+++ workspace/services/modules/plugins/rameses-gov-etracs-waterworks-report-services.jar/sql/waterworks_report_account_status.sql
@@ -0,0 +1,96 @@
+[getReport]
+select 
+	barangay_objid, barangay_name, 
+	sum(res_active) as res_active, sum(com_active) as com_active, 
+	sum(ind_active) as ind_active, sum(bulk_active) as bulk_active,
+	sum(res_inactive) as res_inactive, sum(com_inactive) as com_inactive, 
+	sum(ind_inactive) as ind_inactive, sum(bulk_inactive) as bulk_inactive 
+from ( 
+	select 
+		sn.barangay_objid, sn.barangay_name, 
+		case 
+			when wa.classificationid='RESIDENTIAL' and wm.state <> 'DISCONNECTED' then 1 
+			when wa.classificationid='RESIDENTIAL' and wa.state = 'ACTIVE' then 1 
+			else 0 
+		end as res_active, 
+		case 
+			when wa.classificationid='COMMERCIAL' and wm.state <> 'DISCONNECTED' then 1 
+			when wa.classificationid='COMMERCIAL' and wa.state = 'ACTIVE' then 1 
+			else 0 
+		end as com_active, 
+		case 
+			when wa.classificationid='INDUSTRIAL' and wm.state <> 'DISCONNECTED' then 1 
+			when wa.classificationid='INDUSTRIAL' and wa.state = 'ACTIVE' then 1 
+			else 0 
+		end as ind_active, 
+		case 
+			when wa.classificationid='BULK' and wm.state <> 'DISCONNECTED' then 1 
+			when wa.classificationid='BULK' and wa.state = 'ACTIVE' then 1 
+			else 0 
+		end as bulk_active, 
+		case 
+			when wa.classificationid='RESIDENTIAL' and wm.state = 'DISCONNECTED' then 1 
+			when wa.classificationid='RESIDENTIAL' and wa.state = 'INACTIVE' then 1 
+			else 0 
+		end as res_inactive,  
+		case 
+			when wa.classificationid='COMMERCIAL' and wm.state = 'DISCONNECTED' then 1 
+			when wa.classificationid='COMMERCIAL' and wa.state = 'INACTIVE' then 1 
+			else 0 
+		end as com_inactive,  
+		case 
+			when wa.classificationid='INDUSTRIAL' and wm.state = 'DISCONNECTED' then 1 
+			when wa.classificationid='INDUSTRIAL' and wa.state = 'INACTIVE' then 1 
+			else 0 
+		end as ind_inactive,  
+		case 
+			when wa.classificationid='BULK' and wm.state = 'DISCONNECTED' then 1 
+			when wa.classificationid='BULK' and wa.state = 'INACTIVE' then 1 
+			else 0 
+		end as bulk_inactive  
+	from waterworks_account wa  
+		inner join waterworks_meter wm on wm.objid = wa.meterid 
+		inner join vw_waterworks_stubout_node sn on sn.objid = wa.stuboutnodeid 
+
+	union all 
+
+	select 
+		sn.barangay_objid, sn.barangay_name, 
+		case 
+			when wa.classificationid='RESIDENTIAL' and wa.state = 'ACTIVE' then 1 
+			else 0 
+		end as res_active, 
+		case 
+			when wa.classificationid='COMMERCIAL' and wa.state = 'ACTIVE' then 1 
+			else 0 
+		end as com_active, 
+		case 
+			when wa.classificationid='INDUSTRIAL' and wa.state = 'ACTIVE' then 1 
+			else 0 
+		end as ind_active, 
+		case 
+			when wa.classificationid='BULK' and wa.state = 'ACTIVE' then 1 
+			else 0 
+		end as bulk_active, 
+		case 
+			when wa.classificationid='RESIDENTIAL' and wa.state = 'INACTIVE' then 1 
+			else 0 
+		end as res_inactive,  
+		case 
+			when wa.classificationid='COMMERCIAL' and wa.state = 'INACTIVE' then 1 
+			else 0 
+		end as com_inactive,  
+		case 
+			when wa.classificationid='INDUSTRIAL' and wa.state = 'INACTIVE' then 1 
+			else 0 
+		end as ind_inactive,  
+		case 
+			when wa.classificationid='BULK' and wa.state = 'INACTIVE' then 1 
+			else 0 
+		end as bulk_inactive  
+	from waterworks_account wa  
+		inner join vw_waterworks_stubout_node sn on sn.objid = wa.stuboutnodeid 
+	where wa.meterid is null 
+)t1 
+where barangay_objid is not null ${filters} 
+group by barangay_objid, barangay_name 
