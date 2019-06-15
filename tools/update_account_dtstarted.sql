025b8d1defd71f46320078d0ff5a04ed78785e2b
worgflores <worgflores@gmail.com>
2019-06-15 11:37:09 +0800

    updated

--- tools/update_account_dtstarted.sql
+++ tools/update_account_dtstarted.sql
@@ -1,3 +1,4 @@
+/*
 update 
 	waterworks_account aa, ( 
 		select a.objid, 
@@ -12,3 +13,45 @@ update
 	)bb 
 set aa.dtstarted = bb.startdate 
 where aa.objid = bb.objid 
+*/
+
+-- 1. create temporary table 
+drop table if exists ztmp_account_startdate 
+;
+create table ztmp_account_startdate ( 
+	objid varchar(50) not null, 
+	startdate date null, 
+	primary key (objid) 
+)
+; 
+
+-- 2. insert data  
+insert into ztmp_account_startdate ( 
+	objid, startdate 
+) 
+select objid, dtstarted
+from waterworks_account 
+where dtstarted is null 
+;
+
+
+-- 3. fetch the first reading 
+select a.objid, 
+	(
+		select min(bb.readingdate) as readingdate  
+		from waterworks_billing b 
+			inner join waterworks_batch_billing bb on bb.objid = b.batchid 
+		where b.acctid = a.objid 
+	) as startdate   
+from ztmp_account_startdate a 
+; 
+
+-- 3. update the actual records   
+update waterworks_account aa, ztmp_account_startdate bb 
+set aa.dtstarted = bb.startdate 
+where aa.objid = bb.objid 
+;
+
+-- 4. drop temporary table 
+drop table ztmp_account_startdate
+;
--- workspace/services/modules/plugins/rameses-gov-etracs-waterworks-report-services.jar/sql/waterworks_report_account_status.sql
+++ workspace/services/modules/plugins/rameses-gov-etracs-waterworks-report-services.jar/sql/waterworks_report_account_status.sql
@@ -4,95 +4,55 @@ select
 	sum(res_active) as res_active, sum(com_active) as com_active, 
 	sum(ind_active) as ind_active, sum(bulk_active) as bulk_active,
 	sum(res_inactive) as res_inactive, sum(com_inactive) as com_inactive, 
-	sum(ind_inactive) as ind_inactive, sum(bulk_inactive) as bulk_inactive 
+	sum(ind_inactive) as ind_inactive, sum(bulk_inactive) as bulk_inactive, 
+	sum(gov_active) as gov_active, sum(gov_inactive) as gov_inactive 
 from ( 
 	select 
 		so.barangay_objid, so.barangay_name, 
 		case 
-			when wa.classificationid='RESIDENTIAL' and wm.state <> 'DISCONNECTED' then 1 
-			when wa.classificationid='RESIDENTIAL' and wa.state = 'ACTIVE' then 1 
-			else 0 
+			when wa.classificationid='RESIDENTIAL' and wa.state = 'ACTIVE' and ifnull(wm.state,'') <> 'DISCONNECTED' then 1 else 0 
 		end as res_active, 
 		case 
-			when wa.classificationid='COMMERCIAL' and wm.state <> 'DISCONNECTED' then 1 
-			when wa.classificationid='COMMERCIAL' and wa.state = 'ACTIVE' then 1 
-			else 0 
+			when wa.classificationid='COMMERCIAL' and wa.state = 'ACTIVE' and ifnull(wm.state,'') <> 'DISCONNECTED' then 1 else 0 
 		end as com_active, 
 		case 
-			when wa.classificationid='INDUSTRIAL' and wm.state <> 'DISCONNECTED' then 1 
-			when wa.classificationid='INDUSTRIAL' and wa.state = 'ACTIVE' then 1 
-			else 0 
+			when wa.classificationid='INDUSTRIAL' and wa.state = 'ACTIVE' and ifnull(wm.state,'') <> 'DISCONNECTED' then 1 else 0 
 		end as ind_active, 
 		case 
-			when wa.classificationid='BULK' and wm.state <> 'DISCONNECTED' then 1 
-			when wa.classificationid='BULK' and wa.state = 'ACTIVE' then 1 
-			else 0 
+			when wa.classificationid='BULK' and wa.state = 'ACTIVE' and ifnull(wm.state,'') <> 'DISCONNECTED' then 1 else 0 
 		end as bulk_active, 
 		case 
-			when wa.classificationid='RESIDENTIAL' and wm.state = 'DISCONNECTED' then 1 
+			when wa.classificationid='GOVERNMENT' and wa.state = 'ACTIVE' and ifnull(wm.state,'') <> 'DISCONNECTED' then 1 else 0 
+		end as gov_active, 
+		case 
+			when wa.classificationid='RESIDENTIAL' and ifnull(wm.state,'') = 'DISCONNECTED' then 1 
 			when wa.classificationid='RESIDENTIAL' and wa.state = 'INACTIVE' then 1 
 			else 0 
 		end as res_inactive,  
 		case 
-			when wa.classificationid='COMMERCIAL' and wm.state = 'DISCONNECTED' then 1 
+			when wa.classificationid='COMMERCIAL' and ifnull(wm.state,'') = 'DISCONNECTED' then 1 
 			when wa.classificationid='COMMERCIAL' and wa.state = 'INACTIVE' then 1 
 			else 0 
 		end as com_inactive,  
 		case 
-			when wa.classificationid='INDUSTRIAL' and wm.state = 'DISCONNECTED' then 1 
+			when wa.classificationid='INDUSTRIAL' and ifnull(wm.state,'') = 'DISCONNECTED' then 1 
 			when wa.classificationid='INDUSTRIAL' and wa.state = 'INACTIVE' then 1 
 			else 0 
 		end as ind_inactive,  
 		case 
-			when wa.classificationid='BULK' and wm.state = 'DISCONNECTED' then 1 
+			when wa.classificationid='BULK' and ifnull(wm.state,'') = 'DISCONNECTED' then 1 
 			when wa.classificationid='BULK' and wa.state = 'INACTIVE' then 1 
 			else 0 
-		end as bulk_inactive  
-	from waterworks_account wa  
-		inner join waterworks_meter wm on wm.objid = wa.meterid 
-		inner join waterworks_stubout_node sn on sn.objid = wa.stuboutnodeid  
-		inner join waterworks_stubout so on so.objid = sn.stuboutid 
-
-	union all 
-
-	select 
-		so.barangay_objid, so.barangay_name, 
+		end as bulk_inactive, 
 		case 
-			when wa.classificationid='RESIDENTIAL' and wa.state = 'ACTIVE' then 1 
+			when wa.classificationid='GOVERNMENT' and ifnull(wm.state,'') = 'DISCONNECTED' then 1 
+			when wa.classificationid='GOVERNMENT' and wa.state = 'INACTIVE' then 1 
 			else 0 
-		end as res_active, 
-		case 
-			when wa.classificationid='COMMERCIAL' and wa.state = 'ACTIVE' then 1 
-			else 0 
-		end as com_active, 
-		case 
-			when wa.classificationid='INDUSTRIAL' and wa.state = 'ACTIVE' then 1 
-			else 0 
-		end as ind_active, 
-		case 
-			when wa.classificationid='BULK' and wa.state = 'ACTIVE' then 1 
-			else 0 
-		end as bulk_active, 
-		case 
-			when wa.classificationid='RESIDENTIAL' and wa.state = 'INACTIVE' then 1 
-			else 0 
-		end as res_inactive,  
-		case 
-			when wa.classificationid='COMMERCIAL' and wa.state = 'INACTIVE' then 1 
-			else 0 
-		end as com_inactive,  
-		case 
-			when wa.classificationid='INDUSTRIAL' and wa.state = 'INACTIVE' then 1 
-			else 0 
-		end as ind_inactive,  
-		case 
-			when wa.classificationid='BULK' and wa.state = 'INACTIVE' then 1 
-			else 0 
-		end as bulk_inactive  
+		end as gov_inactive  
 	from waterworks_account wa  
 		inner join waterworks_stubout_node sn on sn.objid = wa.stuboutnodeid  
 		inner join waterworks_stubout so on so.objid = sn.stuboutid 
-	where wa.meterid is null 
+		left join waterworks_meter wm on wm.objid = wa.meterid 
 )t1 
 where barangay_objid is not null ${filters} 
 group by barangay_objid, barangay_name 
